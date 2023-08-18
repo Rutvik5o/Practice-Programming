36888bef5c1beaced91b0f56b91eb5a4104e14d5
Rutvik Prajapati <96343256+Rutvik5o@users.noreply.github.com>
2023-08-18 11:46:57 +0530

    Final Version

---
+++ STARRF.C
@@ -0,0 +1,103 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define N 5
+
+int stack[N];
+int top= -1;
+
+void push()
+{
+  int data;
+
+  if(top==N-1)
+  {
+    printf("\nStack=>UnderFlow");
+  }
+  else
+  {
+   printf("\nPush Element=> ");
+   scanf("%d",&data);
+
+   top++;
+   stack[top]=data;
+  }
+}
+
+void pop()
+{
+  if(top==-1)
+  {
+    printf("\nStack=>UnderFlow");
+  }
+  else
+  {
+    printf("\nPop Element=> %d",stack[top]);
+    top--;
+  }
+}
+
+void peek()
+{
+ if(top==-1)
+ {
+   printf("\nStack=>UnderFlow");
+ }
+ else
+ {
+   printf("\nStack Element=> %d",stack[top]);
+ }
+}
+
+void display()
+{
+  int i;
+
+  if(top==-1)
+  {
+   printf("\nStack=>UnderFlow");
+  }
+  else
+  {
+   printf("\nElement's On Stack=> \n");
+
+   for(i=top;i>=0;i--)
+   {
+     printf("%d\n",stack[i]);
+   }
+  }
+}
+
+int main()
+{
+  int choice;
+
+  do
+  {
+    printf("\nChoose Operation On Stack(Array)=>\n1.Push\n2.Pop\n3.Peek\n4.Display\n5.Exit\n");
+    scanf("%d",&choice);
+
+    switch(choice)
+    {
+      case 1: push();
+	      break;
+
+      case 2: pop();
+	      break;
+
+      case 3: peek();
+	      break;
+
+      case 4: display();
+	      break;
+
+      case 5: exit(0);
+
+      default: printf("Invalid Operation");
+    }
+
+  }while(choice!=0);
+
+  return 0;
+}
+
+
